099b70f47e4478b055c243d9fe0e2143c9a63595
Ga Ming Chan <gmchan@Gas-MacBook-Pro.local>
2022-01-24 19:20:02 +0000

    timer function

--- script.js
+++ script.js
@@ -4,24 +4,45 @@ const gPic = document.getElementById("gPic")
 const timer = document.getElementById("timer")
 const score = document.getElementById("score")
 let scoreBoard = 0
+let gameOver = false
 
 start.addEventListener("click", ()=>{
   start.style.visibility = 'hidden'
   time()
+  randomPick()
 })
 
 const time = () =>{
-  let num = 30
+  let num = 5
   let countDown = setInterval(() =>{
     num--
     if(num === 0){
       clearInterval(countDown)
       start.innerHTML = 'Try again?'
       start.style.visibility = 'initial'
+      gameOver = true
     }
     timer.innerHTML = `Timer: ${num}`
   }, 1000)
 }
+
+const randomPick = () =>{
+  let rPicker = setInterval(() => {
+    let rndGoomba = Math.floor(Math.random() * goomba.length)
+    console.log(rndGoomba);
+    goomba[rndGoomba].classList.add('up')
+    setInterval(() => {
+      goomba[rndGoomba].classList.remove('up')
+    }, 2000)
+    if(gameOver){
+      clearInterval(rPicker)
+      console.log("game over")
+    }
+  }, 1000)
+  
+}
+
+
 
 
 
