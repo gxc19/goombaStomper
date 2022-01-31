c3de631aed463705f86fb2cc6409b4153f6fabab
Ga Ming Chan <gmchan@Gas-MacBook-Pro.local>
2022-01-31 22:44:25 +0000

    added bonk

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
 const start = document.getElementById("start");
-const goomba = document.getElementsByClassName("goomba");
-const gPic = document.getElementById("gPic")
-const timer = document.getElementById("timer")
-const score = document.getElementById("score")
-let scoreBoard = 0
-let gameOver = false
+const goomba = document.querySelectorAll(".goomba");
+// const gPic = document.getElementById("gPic");
+const timer = document.getElementById("timer");
+const score = document.getElementById("score");
+let scoreBoard = 0;
+let gameOver = false;
 
-start.addEventListener("click", ()=>{
-  start.style.visibility = 'hidden'
-  time()
-  randomPick()
-})
+start.addEventListener("click", () => {
+  start.style.visibility = "hidden";
+  time();
+  randomPick();
+  gameOver = false;
+});
 
-const time = () =>{
-  let num = 5
-  let countDown = setInterval(() =>{
-    num--
-    if(num === 0){
-      clearInterval(countDown)
-      start.innerHTML = 'Try again?'
-      start.style.visibility = 'initial'
-      gameOver = true
+const time = () => {
+  let num = 15;
+  let countDown = setInterval(() => {
+    num--;
+    if (num === 0) {
+      clearInterval(countDown);
+      start.innerHTML = "Try again?";
+      start.style.visibility = "initial";
+      gameOver = true;
     }
-    timer.innerHTML = `Timer: ${num}`
-  }, 1000)
-}
+    timer.innerHTML = `Timer: ${num}`;
+  }, 1000);
+};
 
-const randomPick = () =>{
+// randomly selects a goomba to pop up, if timer runs out it stops picking
+const randomPick = () => {
   let rPicker = setInterval(() => {
-    let rndGoomba = Math.floor(Math.random() * goomba.length)
+    let rndGoomba = Math.floor(Math.random() * goomba.length);
     console.log(rndGoomba);
-    goomba[rndGoomba].classList.add('up')
-    setInterval(() => {
-      goomba[rndGoomba].classList.remove('up')
-    }, 2000)
-    if(gameOver){
-      clearInterval(rPicker)
-      console.log("game over")
+
+    goomba[rndGoomba].classList.add("up");
+    setTimeout(() => {
+      goomba[rndGoomba].classList.remove("up");
+    }, 1200);
+    if (gameOver) {
+      clearInterval(rPicker);
+      console.log("game over");
     }
-  }, 1000)
-  
+  }, 1200);
+};
+
+for (let i = 0; i < goomba.length; i++){
+  goomba[i].addEventListener("click", (e) => {
+  goomba[i].src = "images/deadGoomba.png";
+    setTimeout(() => {
+      goomba[i].src = "images/goomba.png";
+    }, 1200);
+    console.log("bonk!");
+  });
 }
-
-
 
 
 
@@ -54,10 +64,8 @@ const randomPick = () =>{
 //     goomba[rndGoomba].style.cursor = "pointer";
 //   }, 1000);
 
-
 // });
 
 // gPic.addEventListener("click", ()=>{
 //   gPic.src="images/deadGoomba.png"
 // })
-
